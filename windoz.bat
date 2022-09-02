ee561eccff3088d3748ef1146bfe500d932d9d00
aa2il <jattili@yahoo.com>
2022-09-02 12:47:17 -0700

    updates

--- windoz.bat
+++ windoz.bat
@@ -4,13 +4,16 @@ echo Notes about how to run wclock on Windoze 10
 echo.
 echo Already should have matplotlib, basemap installed from demos
 echo.
-echo This works ...
+echo This does not work bx we need cartopy on winbloz
 echo    wclock.py
+echo
+echo This works ...
+echo    wclock1.py
 echo.
 echo ... but this does NOT work  :-(
 echo   Problem appears to be with matplotlib
 echo   So we're back to its time to find a replacement for basemap
 echo.
-echo   pyinstaller --onefile wclock.py
-echo   dist\wclock.exe
+echo   pyinstaller --onefile wclock1.py
+echo   dist\wclock1.exe
 echo.
